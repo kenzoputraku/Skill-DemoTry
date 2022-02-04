fea735c887af710f8aa572bd4f907f567693b8d9
kenzoputraku <kenzo270403@gmail.com>
2022-02-03 22:46:53 -0800

    lib + code that fails

---
+++ SkillDemoTester.java
@@ -0,0 +1,13 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTester {
+    @Test 
+    public void substraction(){
+        assertEquals(3,SkillDemo.substraction(6, 2));
+    }
+
+}
